28203fa54d6230cdf339bdcce4696d795183a3b8
biwecka <biwecka@mailbox.org>
2024-08-14 15:22:03 +0200

    feat: implement example of how to use 'plotters'

--- bin/playground/main_plotters_cpu.rs
+++ bin/playground/main_plotters_cpu.rs
@@ -1,140 +1,87 @@
-use std::cmp::Ordering;
+// fn main() {
+//     n_queens::run();
+// }
+
 use piston_window::{EventLoop, PistonWindow, WindowSettings};
 use plotters::prelude::*;
+// use plotters_piston::{draw_piston_window};
+use systemstat::platform::common::Platform;
+use systemstat::System;
 
-const FPS: u32 = 30;
+use std::collections::vec_deque::VecDeque;
+
+const FPS: u32 = 10;
+const LENGTH: u32 = 20;
+const N_DATA_POINTS: usize = (FPS * LENGTH) as usize;
 fn main() {
-    // Communication channel
-    #[derive(Debug)]
-    struct Data {
-        pub index: usize,
-        pub x: f32,
-        pub y: f32,
-    }
-    let (sender, receiver) = std::sync::mpsc::channel::<Data>();
-
-    // Spawn data producing thread.
-    let _data = std::thread::spawn(move || {
-        #[derive(PartialEq, Eq, PartialOrd, Ord)]
-        enum Functions {
-            Square,
-            Sin,
-        }
-
-        impl Functions {
-            fn index(&self) -> usize {
-                match self {
-                    Self::Square => 0,
-                    Self::Sin => 1,
-                }
-            }
-
-            fn value(&self, x: f32) -> f32 {
-                match self {
-                    Self::Square => x * x,
-                    Self::Sin => x.sin(),
-                }
-            }
-        }
-
-        let mut function = Functions::Square;
-        let mut x = -2.;
-        let mut limit = 1.99;
-
-        loop {
-            let _ = sender.send(Data {
-                index: function.index(),
-                x,
-                y: function.value(x),
-            });
-            x += 0.1;
-
-            if x > limit {
-                let _ = sender.send(Data {
-                    index: function.index(),
-                    x,
-                    y: function.value(x),
-                });
-
-                if function == Functions::Square {
-                    function = Functions::Sin;
-                    x = -5.;
-                    limit = 4.99;
-
-                } else {
-                    break;
-                }
-            }
-
-            std::thread::sleep(std::time::Duration::from_millis(100));
-        }
-    });
-
-
     let mut window: PistonWindow = WindowSettings::new("Real Time CPU Usage", [450, 300])
         .samples(4)
         .build()
         .unwrap();
 
+    let sys = System::new();
+
     window.set_max_fps(FPS as u64);
 
-    let mut data_0: Vec<(f32, f32)> = vec![];
-    let mut data_1: Vec<(f32, f32)> = vec![];
+    let mut load_measurement: Vec<_> = (0..FPS).map(|_| sys.cpu_load().unwrap()).collect();
+    let mut epoch = 0;
+    let mut data = vec![];
 
-    // This function is executed exactlx FPS amount of times per second.
+
     while let Some(_) = draw_piston_window(&mut window, |b| {
-        while let Ok(data) = receiver.try_recv() {
-            if data.index == 0 {
-                data_0.push((data.x, data.y));
-            }
-            else if data.index == 1 {
-                data_1.push((data.x, data.y));
-            }
-        }
+        let cpu_loads = load_measurement[epoch % FPS as usize].done()?;
 
         let root = b.into_drawing_area();
         root.fill(&WHITE)?;
 
+        if data.len() < cpu_loads.len() {
+            for _ in data.len()..cpu_loads.len() {
+                data.push(VecDeque::from(vec![0f32; N_DATA_POINTS + 1]));
+            }
+        }
 
-        let d0_x_min = data_0.clone().iter().min_by(|(a, _), (b, _)| a.partial_cmp(b).unwrap_or(Ordering::Less)).unwrap_or(&(0., 0.)).0;
-        let d0_x_max = data_0.clone().iter().max_by(|(a, _), (b, _)| a.partial_cmp(b).unwrap_or(Ordering::Less)).unwrap_or(&(0., 0.)).0;
+        for (core_load, target) in cpu_loads.into_iter().zip(data.iter_mut()) {
+            if target.len() == N_DATA_POINTS + 1 {
+                target.pop_front();
+            }
+            target.push_back(1.0 - core_load.idle);
+        }
 
-        let d1_x_min = data_1.clone().iter().min_by(|(a, _), (b, _)| a.partial_cmp(b).unwrap_or(Ordering::Less)).unwrap_or(&(0., 0.)).0;
-        let d1_x_max = data_1.clone().iter().max_by(|(a, _), (b, _)| a.partial_cmp(b).unwrap_or(Ordering::Less)).unwrap_or(&(0., 0.)).0;
+        let mut cc = ChartBuilder::on(&root)
+            .margin(10)
+            .caption("Real Time CPU Usage", ("sans-serif", 30))
+            .x_label_area_size(40)
+            .y_label_area_size(50)
+            .build_cartesian_2d(0..N_DATA_POINTS as u32, 0f32..1f32)?;
 
-        let x_min = f32::min(d0_x_min, d1_x_min).floor();
-        let x_max = f32::max(d0_x_max, d1_x_max).ceil();
+        cc.configure_mesh()
+            .x_label_formatter(&|x| format!("{}", -(LENGTH as f32) + (*x as f32 / FPS as f32)))
+            .y_label_formatter(&|y| format!("{}%", (*y * 100.0) as u32))
+            .x_labels(15)
+            .y_labels(5)
+            .x_desc("Seconds")
+            .y_desc("% Busy")
+            .axis_desc_style(("sans-serif", 15))
+            .draw()?;
 
-        let mut chart = ChartBuilder::on(&root)
-            .caption("y = x^2", ("sans-serif", 20))
-            .margin(5)
-            .x_label_area_size(30)
-            .y_label_area_size(30)
-            .build_cartesian_2d(x_min..x_max, -1.1f32..4.1f32)?;
+        for (idx, data) in (0..).zip(data.iter()) {
+            cc.draw_series(LineSeries::new(
+                (0..).zip(data.iter()).map(|(a, b)| (a, *b)),
+                &Palette99::pick(idx),
+            ))?
+            .label(format!("CPU {}", idx))
+            .legend(move |(x, y)| {
+                Rectangle::new([(x - 5, y - 5), (x + 5, y + 5)], &Palette99::pick(idx))
+            });
+        }
 
-        chart.configure_mesh().draw()?;
-
-
-        // Draw data_0
-        chart
-            .draw_series(LineSeries::new(data_0.clone(), &Palette99::pick(0)))?
-            .label("y = x * x")
-            .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 10, y)], &Palette99::pick(0)));
-
-        // Draw data_1
-        chart
-            .draw_series(LineSeries::new(data_1.clone(), &Palette99::pick(1)))?
-            .label("y = sin(x)")
-            .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 10, y)], &Palette99::pick(1)));
-
-        chart
-            .configure_series_labels()
+        cc.configure_series_labels()
             .background_style(&WHITE.mix(0.8))
             .border_style(&BLACK)
             .draw()?;
 
-        root.present()?;
-
+        load_measurement[epoch % FPS as usize] = sys.cpu_load()?;
+        epoch += 1;
         Ok(())
     }) {}
 }
